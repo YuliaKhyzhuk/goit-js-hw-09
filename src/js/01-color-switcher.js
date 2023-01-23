fdeaf12be8c45930287f96aa8ce6615f45626875
YuliaKhyzhuk <anastezy1@gmail.com>
2023-01-23 05:11:58 +0200

    Виконано завдання 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const bodyEl = document.querySelector('body');
+const startButtonEl = document.querySelector('[data-start]');
+const stopButtonEl = document.querySelector('[data-stop]');
+let intervalID = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const onStartButtonClick = event => {
+  startButtonEl.setAttribute('disabled', '');
+  intervalID = setInterval(() => {
+    const color = getRandomHexColor();
+    bodyEl.style.backgroundColor = color;
+  }, 1000);
+};
+
+const onStopButtonClick = event => {
+  clearInterval(intervalID);
+  startButtonEl.removeAttribute('disabled');
+};
+
+startButtonEl.addEventListener('click', onStartButtonClick);
+stopButtonEl.addEventListener('click', onStopButtonClick);

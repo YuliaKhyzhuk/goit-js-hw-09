15e8a7d7efc39284df6de2570c7533f98fa422b2
YuliaKhyzhuk <anastezy1@gmail.com>
2023-01-24 06:35:36 +0200

    Завдання 3 виконано частково

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,45 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+const promiseFormEl = document.querySelector('.form');
+const submitButtonEl = document.querySelector('[type="submit"]');
+
+const onSubmitBtnClick = event => {
+  event.preventDefault();
+
+  const formData = new FormData(promiseFormEl);
+
+  const submittedData = {};
+  formData.forEach((value, name) => {
+    submittedData[name] = Number(value);
+  });
+
+  let { delay, step, amount } = submittedData;
+
+  for (let i = 1; i <= amount; i += 1) {
+    submittedData.position = i;
+    console.log(submittedData);
+    createPromise(submittedData.position, delay);
+    delay += step;
   }
-}
+
+  function createPromise(position, delay) {
+    const shouldResolve = Math.random() > 0.3;
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (shouldResolve) {
+          resolve(submittedData);
+        } else {
+          reject(submittedData);
+        }
+      }, delay);
+    });
+
+    promise
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+};
+
+submitButtonEl.addEventListener('click', onSubmitBtnClick);
